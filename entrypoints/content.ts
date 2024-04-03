56e75c0fecfa88a6b9faa4f3abeda08a34eb9cc9
Cedric Agoliki <agolikicedric@gmail.com>
2024-04-03 22:16:16 +0000

    remove modal

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,18 @@
 import { REDIRECT_TO_OLD } from "./settings";
 import { storage } from 'wxt/storage'
 
+
+// This function will remove
+// eventual popups on the interface
+function cleanInterface() {
+  // remove overflow on body and reset pointer events to auto
+  document.body.style.overflow = 'auto';
+  document.body.style.pointerEvents = 'auto';
+
+  //For contant marked as NSFW, remove the modal
+  document.querySelector('shreddit-async-loader[bundlename="desktop_rpl_nsfw_blocking_modal"]')?.remove()
+}
+
 export default defineContentScript({
   matches: ['https://www.reddit.com/*'],
   main() {
@@ -9,6 +21,8 @@ export default defineContentScript({
         const pathname = window.location.pathname;
         const oldRedditHost = "https://old.reddit.com";
         window.location.replace(`${oldRedditHost}${pathname}`)
+      } else {
+        cleanInterface()
       }
     })
   },
